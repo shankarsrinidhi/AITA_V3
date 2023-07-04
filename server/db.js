69e2d2a0046c495660c1e15f67f0b98eea32f593
shankarsrinidhi <123007528+shankarsrinidhi@users.noreply.github.com>
2023-07-04 07:45:31 -0400

    committing changes

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "Malavika@95",
+    password: "fdfg",
     host: "localhost",
     port: 5432,
     database: "AITA_V1"
